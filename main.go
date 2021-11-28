2e61e376d255c2aa7ea802bbeeb896162fd56a1f
Pedro Britto <pedro7britto@gmail.com>
2021-11-28 11:11:20 -0300

    Project CRUD created

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"crud/servidor"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+/* CRUD - CREATE, READ, UPDATE, DELETE
+CREATE - PUT
+READ - GET
+UPDADE - PUT
+DELETE - DELET
+*/
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+	router.HandleFunc("/usuarios", servidor.BuscarUsuarios).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.BuscarUsuario).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.AtualizarUsuario).Methods(http.MethodPut)
+	router.HandleFunc("/usuarios/{id}", servidor.DeletarUsuario).Methods(http.MethodDelete)
+
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+
+}
